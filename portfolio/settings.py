fc5df0753a545da399c480aea5c8d16ce0ed3343
PBDE <140658774+PhilBDE@users.noreply.github.com>
2024-01-01 18:52:59 +0000

    Added logging

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -151,3 +151,35 @@ SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", default=True)
 
 SESSION_COOKIE_SECURE = env.bool("SESSION_COOKIE_SECURE", default=True)
 CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", default=True)
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": "logs.log",
+            "formatter": "verbose",
+            "filters": ["require_debug_false"]
+        }
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True
+        }
+    }, 
+    "formatters": {
+        "verbose": {
+            "format": "\n\n{levelname} {asctime} {module} {process:d} {thread:d} \n{message}",
+            "style": "{",
+        }
+    },
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse"
+        }
+    }
+}
