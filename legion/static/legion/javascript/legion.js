b5ff4bb4c2fb390d7516b8908b8b9f13ce3d984c
PBDE <140658774+PhilBDE@users.noreply.github.com>
2024-10-29 00:51:23 +0000

    Added links to DOM objects

--- legion/static/legion/javascript/legion.js
+++ legion/static/legion/javascript/legion.js
@@ -1,5 +1,18 @@
+'use-strict';
 
 // set references to DOM objects
+
+const totalScoreDisplay = document.querySelector('#total-score');
+const currentScoreDisplay = document.querySelector('#current-score');
+
+const rollButton = document.querySelector('#roll-btn');
+const endButton = document.querySelector('#end-btns');
+
+const soloButton = document.querySelector('#solo-btn');
+const pandpButton = document.querySelector('#pandp-btn');
+const aiButton = document.querySelector('#ai-btn');
+
+const dice = document.querySelectorAll('.dice');
 
 // class Die
 
