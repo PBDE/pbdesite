edddfa465f44a31e4ecfbac6acfe1a0647aecb2d
PBDE <140658774+PhilBDE@users.noreply.github.com>
2025-03-19 21:33:31 +0000

    Added Legion link to home page

--- legion/unit_tests/sum.test.js
+++
@@ -1,7 +0,0 @@
-// sum.test.js
-import { expect, test } from 'vitest'
-import { sum } from '../static/legion/javascript/sum.js'
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
