6766f7e17ab0b9a0f8fe1747c405ebd6a911fa7e
PBDE <140658774+PhilBDE@users.noreply.github.com>
2025-05-07 22:58:36 +0100

    hours_tracker: add layout and index templates

--- hours_tracker/views.py
+++ hours_tracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+INDEX_TEMPLATE = "hours_tracker/index.html"
+
 def index(request):
-    return HttpResponse("Hours Tracker")
+    return render(request, INDEX_TEMPLATE)
