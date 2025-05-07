62f06ee2cafcfeb55e8f03c11dd6c9c018b261c1
PBDE <140658774+PhilBDE@users.noreply.github.com>
2025-05-07 23:39:30 +0100

    hours_tracker: replace Index view with IndexView class

--- hours_tracker/views.py
+++ hours_tracker/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views import View
+import json
 
 INDEX_TEMPLATE = "hours_tracker/index.html"
 
-def index(request):
-    return render(request, INDEX_TEMPLATE)
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, INDEX_TEMPLATE)
+
+    def post(self, request):
+
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponseBadRequest("Invalid JSON")
+        
+        # check which form was sent
+
+        return render(request, INDEX_TEMPLATE)
