fccba6118dcbe98c547704e977a974138201bfc0
PBDE <140658774+PhilBDE@users.noreply.github.com>
2025-05-14 21:34:24 +0100

    hours_tracker: add test_hours_tracker.py

---
+++ hours_tracker/functional_tests/test_hours_tracker.py
@@ -0,0 +1,27 @@
+from home_page.functional_tests.base import FunctionalTest
+
+class HoursTrackerTest(FunctionalTest):
+
+    def test_start_day(self):
+
+        ...
+
+    def test_start_break(self):
+
+        ...
+
+    def test_start_break(self):
+
+        ...
+
+    def test_end_day(self):
+
+        ...
+
+    def test_tables(self):
+
+        ...
+
+    def test_correction(self):
+
+        ...
